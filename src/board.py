6e0130b0376644f95ae142a7a7ad0c7f5ade7e86
blazejjurczyk <blazej60@gmail.com>
2025-09-26 18:08:03 +0100

    board setup complete

---
+++ src/board.py
@@ -0,0 +1,37 @@
+class Board:
+    def __init__(self):
+        self.size = 10
+        self.rows = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        self.grid = [['.' for i in range(self.size)] for i in range(self.size)]
+
+    def render(self):
+        # header row (pad 3 spaces so row numbers fit underneath)
+        print("   " + " ".join(self.columns))
+
+        row_number = 1
+        for row in self.grid:
+            # use rjust(2) so "1" becomes " 1" and lines up with "10"
+            print(str(row_number).rjust(2), ' '.join(row))
+            row_number += 1
+        return
+
+board = Board()
+board.render()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
