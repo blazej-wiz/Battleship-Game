3c95cab08e03436441f90f0834ba1d1ad53aa2c1
blazejjurczyk <blazej60@gmail.com>
2025-09-27 02:41:12 +0100

    2 player functionality added

--- src/game.py
+++ src/game.py
@@ -1,12 +1,14 @@
 from src.board import Board
 from src.battleships import Ship
 
-fleet = [('Destroyer', 3), ('Alpha', 5)]
+fleet1 = [('Destroyer', 3)]
+fleet2 = [('Bob', 2)]
 
 player_1 = input('Enter Player 1 name:')
 player_2 = input('Enter Player 2 name:')
 
 board_p1 = Board()
+board_p2 = Board()
 
 
 def prompt_for_coord(board):
@@ -53,6 +55,46 @@ def place_fleet(board, player_name, fleet):
     for ship_name, length in fleet:
         place_one_ship(board, player_name, ship_name, length)
         board.render()
+
+
+# place_fleet(board_p1, player_1, fleet1)
+# place_fleet(board_p2, player_2, fleet2)
+
+def prompt_for_fire(board):
+    while True:
+        coord_input = prompt_for_coord(board)
+        if not board.get(coord_input) == 'X' and not board.get(coord_input) == 'O':
+            return coord_input
+        else:
+            print('Already fired at that coord, try again')
+
+def take_turn(attacker_name, defender_board):
+    print('{player_name} turn'.format(player_name = attacker_name))
+    coord = prompt_for_fire(defender_board)
+    result = defender_board.fire_at(coord)
+    print('{player_name} fired at', coord, ':', result.format(player_name = attacker_name))
+    defender_board.render()
+
+def all_sunk(board):
+    for ship in board.ships:
+        if not ship.is_sunk():
+            return False
+    return True
+
+
+attacker_name = player_1
+defender_board = board_p2
+while True:
+    take_turn(attacker_name, defender_board)
+    if all_sunk(defender_board):
+        print('Game Over! {player_name} wins!'.format(player_name = attacker_name))
+        break
+    if attacker_name == player_1 and defender_board == board_p2:
+        attacker_name = player_2
+        defender_board = board_p1
+    else:
+        attacker_name = player_1
+        defender_board = board_p2
 
 
 
